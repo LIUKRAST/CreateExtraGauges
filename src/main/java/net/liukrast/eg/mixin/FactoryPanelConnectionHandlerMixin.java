9fd0e83f048cb80a4185107646a71177d29e9df8
LiukRast <darthlucavader@gmail.com>
2025-05-11 16:47:38 +0200

    Removed useless imports

--- src/main/java/net/liukrast/eg/mixin/FactoryPanelConnectionHandlerMixin.java
+++ src/main/java/net/liukrast/eg/mixin/FactoryPanelConnectionHandlerMixin.java
@@ -1,13 +1,10 @@
 package net.liukrast.eg.mixin;
 
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.simibubi.create.content.logistics.factoryBoard.FactoryPanelBehaviour;
 import com.simibubi.create.content.logistics.factoryBoard.FactoryPanelConnectionHandler;
 import net.liukrast.eg.api.logistics.board.AbstractPanelBehaviour;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
-import org.checkerframework.checker.units.qual.A;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
