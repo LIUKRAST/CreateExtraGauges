2a55dd11932d393342645e17a6efa16d9a5e3b9e
LiukRast <darthlucavader@gmail.com>
2025-08-22 13:04:53 +0200

    Dumb push

--- src/main/java/net/liukrast/eg/mixin/PackageRepackageHelperMixin.java
+++ src/main/java/net/liukrast/eg/mixin/PackageRepackageHelperMixin.java
@@ -1,24 +1,20 @@
 package net.liukrast.eg.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.simibubi.create.content.logistics.BigItemStack;
 import com.simibubi.create.content.logistics.packager.InventorySummary;
 import com.simibubi.create.content.logistics.packager.repackager.PackageRepackageHelper;
-import com.simibubi.create.content.logistics.stockTicker.PackageOrderWithCrafts;
-import net.liukrast.eg.TEST;
-import net.minecraft.util.RandomSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
 @Mixin(PackageRepackageHelper.class)
 public class PackageRepackageHelperMixin {
 
-    @Inject(method = "repackBasedOnRecipes", at = @At("HEAD"), cancellable = true)
-    private void repackBasedOnRecipes(InventorySummary summary, PackageOrderWithCrafts order, String address, RandomSource r, CallbackInfoReturnable<List<BigItemStack>> cir) {
-        cir.setReturnValue(TEST.repackBasedOnRecipes(summary, order, address, r));
-        cir.cancel();
+    @ModifyExpressionValue(method = "repackBasedOnRecipes", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/logistics/stockTicker/PackageOrder;stacks()Ljava/util/List;", ordinal = 1))
+    private List<BigItemStack> repackBasedOnRecipes(List<BigItemStack> original, @Local(argsOnly = true) InventorySummary summary) {
+        return summary.getStacks();
     }
 }
