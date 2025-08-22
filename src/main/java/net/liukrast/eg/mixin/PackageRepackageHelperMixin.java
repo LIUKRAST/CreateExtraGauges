04eb60c556423ec4bdaa7b403e79872b7466604d
LiukRast <darthlucavader@gmail.com>
2025-08-22 13:39:28 +0200

    I said i fixed it but apparently I didnt

--- src/main/java/net/liukrast/eg/mixin/PackageRepackageHelperMixin.java
+++ src/main/java/net/liukrast/eg/mixin/PackageRepackageHelperMixin.java
@@ -1,10 +1,13 @@
 package net.liukrast.eg.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.simibubi.create.content.logistics.BigItemStack;
 import com.simibubi.create.content.logistics.packager.InventorySummary;
 import com.simibubi.create.content.logistics.packager.repackager.PackageRepackageHelper;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -16,5 +19,10 @@ public class PackageRepackageHelperMixin {
     @ModifyExpressionValue(method = "repackBasedOnRecipes", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/logistics/stockTicker/PackageOrder;stacks()Ljava/util/List;", ordinal = 1))
     private List<BigItemStack> repackBasedOnRecipes(List<BigItemStack> original, @Local(argsOnly = true) InventorySummary summary) {
         return summary.getStacks();
+    }
+
+    @WrapOperation(method = "repackBasedOnRecipes", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;copyWithCount(I)Lnet/minecraft/world/item/ItemStack;", ordinal = 0))
+    private ItemStack repackBasedOnRecipes(ItemStack instance, int i, Operation<ItemStack> original) {
+        return instance.copy();
     }
 }
