3fb94e98888512161f091d053e577400c856b695
JustAGuy444 <281965879+JustAGuy4447@users.noreply.github.com>
2026-06-06 15:17:15 +0200

    Removed unused imports > Why do I always forget to do that?

--- src/main/java/net/liukrast/eg/content/logistics/LinkedLeverBlock.java
+++ src/main/java/net/liukrast/eg/content/logistics/LinkedLeverBlock.java
@@ -8,8 +8,6 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
 import org.lwjgl.system.NonnullDefault;
