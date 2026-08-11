89fa8450d9d2e834c6de1e22088815287dd188f1
JustAGuy444 <281965879+JustAGuy4447@users.noreply.github.com>
2026-08-11 16:00:55 +0200

    removed unused imports

--- src/main/java/net/liukrast/eg/mixin/ValueSettingsInputHandlerMixin.java
+++ src/main/java/net/liukrast/eg/mixin/ValueSettingsInputHandlerMixin.java
@@ -1,19 +1,15 @@
 package net.liukrast.eg.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.simibubi.create.foundation.blockEntity.behaviour.ValueSettingsInputHandler;
 import com.simibubi.create.foundation.utility.AdventureUtil;
 import net.liukrast.eg.content.logistics.link.LinkedControlBlock;
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ValueSettingsInputHandler.class)
